c93c454af7924ac54bf8d8ab2b41e7417603dfc8
Richard Smith-Unna <richardsmith404@gmail.com>
2016-02-11 14:53:13 +0000

    Script to archive a pad

---
+++ archive.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# make output directory
+echo "Removing protocol from URL"
+proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+url="$(echo ${1/$proto/})"
+echo "Creating directory $url"
+mkdir -p $url
+
+# download
+echo "Downloading etherpad archive to $url/index.html"
+curl "$1/export/html" --output "$url/index.html"
+echo "Done :)"
